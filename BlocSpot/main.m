e65a75112fa7df949bd563838caf85901ebd0825
Sameer Totey <sameertotey@Sameers-MacBook-Pro.local>
2014-12-03 09:44:45 -0700

    Initial Commit

---
+++ BlocSpot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocSpot
+//
+//  Created by Sameer Totey on 12/3/14.
+//  Copyright (c) 2014 Sameer Totey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
